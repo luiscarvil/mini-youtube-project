0d3a63f264d93f21d0888494c4ef3ebc037c272c
luiscarvil <luiscarvajal05@gmail.com>
2021-10-09 23:45:30 -0500

    remove keys jwt

--- backend/src/keys/generate-keys.sh
+++
@@ -1,5 +0,0 @@
-cd keys
-ssh-keygen -t rsa -b 2048 -m PEM -f jwtRS256.key
-openssl rsa -in jwtRS256.key -pubout -outform PEM -out jwtRS256.key.pub
-ssh-keygen -t rsa -b 2048 -m PEM -f app-key.key
-openssl rsa -in  app-key.key -pubout -outform PEM -out app-key.key.pub
